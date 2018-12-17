9c72ad04dd48b77cd55cd3777c05726af3ca883b
Evan <evanmlongwood@gmail.com>
2018-12-17 14:03:31 -0800

    Returns random image of a doggo.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 function add_doggo() {
-    fetch("https://dog.ceo/api/breed/shiba/images")
+    fetch("https://dog.ceo/api/breed/shiba/images/random")
         .then(function(response) {
             return response.json();
         })
         .then(function(imageObj) {
-            show_image(imageObj.message[0], 500,500, "Doggo");
+            show_image(imageObj.message, 500,500, "Doggo");
         });
 }
 
 
 function show_image(src, width, height, alt) {
-    var img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = src;
     img.width = width;
     img.height = height;
     img.alt = alt;
-    document.body.appendChild(img);
+    img.id = 'doggo_image';
+
+    const current_image = document.getElementById(img.id);
+    if (current_image) {
+        document.body.replaceChild(img, current_image);
+    } else {
+        document.body.appendChild(img);
+    }
 }
