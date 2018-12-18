d8c1855e4970239a7b70698d0c9f26577b0c23f0
Evan <evanmlongwood@gmail.com>
2018-12-18 11:44:44 -0800

    Another clean refractor.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
 function add_doggo() {
     fetch("https://dog.ceo/api/breed/shiba/images/random")
         .then(response => response.json())
-        .then(function(dogsObj) {
-            show_image(dogsObj.message, 500,500, "Doggo");
-        });
+        .then(show_image);
 }
 
 
-function show_image(src, width, height, alt) {
+function show_image(dogObj) {
     const img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
+    img.src = dogObj.message;
+    img.width = 500;
+    img.height = 500;
+    img.alt = "doggo";
     img.id = 'doggo_image';
 
     const current_image = document.getElementById(img.id);
