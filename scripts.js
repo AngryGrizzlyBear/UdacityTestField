87367d56badb83be157b7a045bc8c06c3667029f
Evan <evanmlongwood@gmail.com>
2018-12-21 08:12:01 -0800

    deleted comment out.

--- scripts.js
+++ scripts.js
@@ -7,11 +7,6 @@ function add_doggo() {
 function show_image({message}) {
     const current_image = document.getElementById("static-doggo");
     current_image.src = message
-
-    // I've decided to keep this for research purposes. Per Dennis, I can use git blame!
-// function show_image(dogObj) {
-//     const current_image = document.getElementById("static-doggo");
-//     current_image.src = dogObj.message
 }
 
 add_doggo();
